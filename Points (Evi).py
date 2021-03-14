cb4d03efe2618a7005e8c1cdab1196b620cfe37a
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2021-03-15 10:13:19 +1300

    printed individual match scores and total score

---
+++ Points (Evi).py
@@ -0,0 +1,21 @@
+#printing match scores
+
+match_1 = 3
+print("The score for match one: {}".format(match_1))
+
+match_2 = 6
+print("The score for match two: {}".format(match_2))
+
+match_3 = 10
+print("The score for match three: {}".format(match_3))
+
+match_4 = 10
+print("The score for match four: {}".format(match_4))
+
+match_5 = 6
+print("The score for match five: {}".format(match_5))
+
+#total match score
+total_score = match_1 + match_2 + match_3 + match_4 + match_5
+print("The total score for the five matches: {}".format(total_score))
+
