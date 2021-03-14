08c97a55756f2b21fd10398d3652d46731268757
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2021-03-15 10:24:38 +1300

    printed gift card balance before and after transaction

---
+++ Gift card (Evi).py
@@ -0,0 +1,9 @@
+#variables
+original_balance = 50
+game_cost = 35
+new_balance = original_balance - game_cost
+
+
+print("Your gift card balance is ${}".format(original_balance))
+print("Your gift card balance is ${} after purchases".format(new_balance))
+
