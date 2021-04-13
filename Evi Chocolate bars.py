7125df2f2f2a2deab56024da8c2d0a48bc8e225a
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2021-04-13 14:59:34 +1200

    simplified print statement

--- Evi Chocolate bars.py
+++ Evi Chocolate bars.py
@@ -3,6 +3,4 @@ people = 5
 whole_bars = choco_bars // people
 extra_bars = choco_bars % 7
 left_over =  whole_bars * 7 % people
-print("Whole chocolate bars each: {}".format(whole_bars))
-print("Extra squares each: {}".format(extra_bars))
-print("Left over squares: {}".format(left_over))
+print("Whole chocolate bars each: {} \n Extra squares each: {} \n Left over squares: {}".format(whole_bars, extra_bars, left_over))
